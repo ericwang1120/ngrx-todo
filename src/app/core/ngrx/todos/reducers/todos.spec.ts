e00d4436f66fad590807374c9a1d1bbf06913856
Eric Wang <xiaobica@qq.com>
2017-11-29 11:09:01 +1300

    reducer test cases

--- src/app/core/ngrx/todos/reducers/todos.spec.ts
+++ src/app/core/ngrx/todos/reducers/todos.spec.ts
@@ -1,7 +1,22 @@
 import { reducer } from './todos';
 import * as fromTodos from './todos';
-import { Load, LoadFail, LoadSuccess, AddOneSuccess } from '../actions/todo';
+import {
+    Load,
+    LoadFail,
+    LoadSuccess,
+    AddOneSuccess,
+    AddOne,
+    AddOneFail,
+    EditOne,
+    EditOneSuccess,
+    EditOneFail,
+    RemoveOneSuccess,
+    RemoveOne,
+    RemoveOneFail,
+    RemoveMany
+} from '../actions/todo';
 import { Todo, generateMockTodo } from '../models/todo';
+import { RemoveManySuccess, RemoveManyFail, SelectTodo } from '../actions/todo';
 
 describe('TodosReducer', () => {
     const todo1 = generateMockTodo();
@@ -77,10 +92,31 @@ describe('TodosReducer', () => {
     describe('selectors', () => {
         const expectedResult = initialState;
 
-        it('should return correct selector', () => {
+        it('should return correct loading selector', () => {
             const loading = expectedResult.loading;
 
             expect(fromTodos.getLoading(expectedResult)).toEqual(loading);
+        });
+
+        it('should return correct selected id selector', () => {
+            const selectedId = expectedResult.selectedId;
+
+            expect(fromTodos.getSelectedId(expectedResult)).toEqual(selectedId);
+        });
+    });
+
+    describe('ADD_ONE', () => {
+        const expectedResult = {
+            ...initialState,
+            loading: true,
+        };
+
+        it('should change loading to true when adding one', () => {
+            const action = new AddOne(todo2);
+
+            const result = reducer(initialState, action);
+
+            expect(result).toEqual(expectedResult);
         });
     });
 
@@ -96,6 +132,347 @@ describe('TodosReducer', () => {
             const action = new AddOneSuccess(todo2);
 
             const result = reducer(initialState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('ADD_ONE_FAIL', () => {
+        const expectedResult = {
+            ids: [],
+            entities: {},
+            loading: false,
+            selectedId: 0
+        };
+
+        it('return previous list of todos when load fail', () => {
+            const action = new AddOneFail('Error Message');
+
+            const result = reducer(initialState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('EDIT_ONE', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        const changes = { title: 'editedTitle' };
+        const idToEdit = 222;
+
+        const editedObj = {
+            ...todo2,
+            title: 'editedTitle'
+        };
+
+        it('should change loading to true when editing one', () => {
+            const expectedResult = {
+                ids: [todo2.id, todo3.id],
+                entities: {
+                    [todo2.id]: todo2,
+                    [todo3.id]: todo3
+                },
+                loading: true,
+                selectedId: 0
+            };
+
+            const action = new EditOne(idToEdit, changes);
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+
+    describe('EDIT_ONE_SUCCESS', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const changes = { title: 'editedTitle' };
+        const idToEdit = 222;
+
+        const editedObj = {
+            ...todo2,
+            title: 'editedTitle'
+        };
+
+        const expectedResult = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: editedObj,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        it('should update state when edit success', () => {
+
+            const action = new EditOneSuccess({ id: idToEdit, changes });
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('EDIT_ONE_FAIL', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const expectedResult = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        it('return previous list of todos when edit fail', () => {
+            const action = new EditOneFail('Error Message');
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('REMOVE_ONE', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        const idToRemove = 222;
+
+        it('should change loading to true when removing one', () => {
+            const expectedResult = {
+                ids: [todo2.id, todo3.id],
+                entities: {
+                    [todo2.id]: todo2,
+                    [todo3.id]: todo3
+                },
+                loading: true,
+                selectedId: 0
+            };
+
+            const action = new RemoveOne(idToRemove);
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('REMOVE_ONE_SUCCESS', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const idToRemove = 222;
+
+        const expectedResult = {
+            ids: [todo3.id],
+            entities: {
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        it('should update state when remove one success', () => {
+
+            const action = new RemoveOneSuccess(idToRemove);
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('REMOVE_ONE_FAIL', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const expectedResult = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        it('return previous list of todos when remove fail', () => {
+            const action = new RemoveOneFail('Error Message');
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('REMOVE_MANY', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        const idsToRemove = [222, 333];
+
+        it('should change loading to true when removing many', () => {
+            const expectedResult = {
+                ids: [todo2.id, todo3.id],
+                entities: {
+                    [todo2.id]: todo2,
+                    [todo3.id]: todo3
+                },
+                loading: true,
+                selectedId: 0
+            };
+
+            const action = new RemoveMany(idsToRemove);
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('REMOVE_MANY_SUCCESS', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const idsToRemove = [222, 333];
+
+        const expectedResult = {
+            ids: [],
+            entities: {
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        it('should update state when remove many success', () => {
+
+            const action = new RemoveManySuccess(idsToRemove);
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('REMOVE_MANY_FAIL', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const idsToRemove = [222, 333];
+
+        const expectedResult = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: false,
+            selectedId: 0
+        };
+
+        it('return previous list of todos when remove many fail', () => {
+            const action = new RemoveManyFail('Error Message');
+
+            const result = reducer(currentState, action);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('SELECT_TODO', () => {
+        const currentState = {
+            ids: [todo2.id, todo3.id],
+            entities: {
+                [todo2.id]: todo2,
+                [todo3.id]: todo3
+            },
+            loading: true,
+            selectedId: 0
+        };
+
+        const idsToSelect = 222;
+
+        const expectedResult = {
+            ...currentState,
+            selectedId: 222
+        };
+
+        it('should select the todo', () => {
+            const action = new SelectTodo(idsToSelect);
+
+            const result = reducer(currentState, action);
 
             expect(result).toEqual(expectedResult);
         });
